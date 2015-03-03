19bd11ef73bc876190263f0f8e7b0fe2daf4d49f
Robin Schroer <sulami@peerwire.org>
2015-03-03 22:44:18 +0100

    add getopts stuff

--- src/wm.c
+++ src/wm.c
@@ -1,6 +1,29 @@
+#include <getopt.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <wayland-util.h>
+
+#define VERSION "0.1a"
+
 int
-main()
+main(int argc, char *argv[])
 {
-    return 0;
+	int opt;
+
+	while((opt = getopt(argc, argv, "vh")) != -1) {
+		switch (opt) {
+		case 'h':
+			puts("TBD");
+			break;
+		case 'v':
+			printf("%s %s\n", argv[0], VERSION);
+			break;
+		default:
+			fprintf(stderr, "Usage: %s [-vh]\n", argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	return 0;
 }
 
