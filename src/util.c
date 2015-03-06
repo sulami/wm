174a566901ae0dba2ed062ba1cb413c9b477ea41
Robin Schroer <sulami@peerwire.org>
2015-03-06 13:39:07 +0100

    add spawn()

--- src/util.c
+++ src/util.c
@@ -1,10 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <wayland-client.h>
 
 void
 die(char *err)
 {
 	fprintf(stderr, "ERROR: %s.\n", err);
 	exit(EXIT_FAILURE);
+}
+
+void
+spawn(void *data, uint32_t time, uint32_t value, uint32_t state)
+{
+	char * const *command = data;
+
+	if (time || value) {} /* FIXME shut up compiler */
+
+	if (state != WL_KEYBOARD_KEY_STATE_PRESSED)
+		return;
+
+	if (fork() == 0) {
+		execvp(command[0], command);
+		exit(EXIT_FAILURE);
+	}
 }
 
--- src/util.h
+++ src/util.h
@@ -1,10 +1,17 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <stdint.h>
+
 /*
  * Print an pretty formatted error message and then kill everything.
  */
 void die(char *);
+
+/*
+ * Spawn an arbitrary command.
+ */
+void spawn(void *, uint32_t, uint32_t, uint32_t);
 
 #endif
 
